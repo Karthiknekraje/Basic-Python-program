98f51038d95000a1a56a3f22c0549d1b52e97576
coder <147401359+Karthiknekraje@users.noreply.github.com>
2024-06-13 22:51:50 +0530

    Create dictionaryloop.py

---
+++ dictionaryloop.py
@@ -0,0 +1,10 @@
+
+capitals = {
+    "France": "Paris",
+    "Italy": "Rome",
+    "Germany": "Berlin"
+}
+
+
+for country, capital in capitals.items():
+    print(f"The capital of {country} is {capital}")
