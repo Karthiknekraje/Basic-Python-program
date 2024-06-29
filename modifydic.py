9895fd05479450d067236fbd5786c8cbe7bf1406
coder <147401359+Karthiknekraje@users.noreply.github.com>
2024-06-30 00:32:26 +0530

    Update

---
+++ modifydic.py
@@ -0,0 +1,7 @@
+# Add a new key-value pair
+student_grades["David"] = "B"
+print("After adding David:", student_grades)
+
+# Modify an existing value
+student_grades["Alice"] = "A+"
+print("After modifying Alice's grade:", student_grades)
